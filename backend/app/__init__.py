5e68bab12d9afed78238bb01f7719ac16c8ee059
FlacSy <flacsy.x@gmail.com>
2024-12-07 12:24:41 +0100

    Update error handling and add error.html

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .routes.index import index_bp
 from .routes.download import download_bp
-
+from .routes.error import error_bp
+# from .routes.auth import ...
+# from .routes.user import ...
 
 def create_app():
     app = Flask(__name__, 
@@ -10,4 +12,5 @@ def create_app():
             )
     app.register_blueprint(index_bp)
     app.register_blueprint(download_bp)
+    app.register_blueprint(error_bp)
     return app
--- backend/app/routes/error.py
+++ backend/app/routes/error.py
@@ -3,12 +3,11 @@ from flask import Blueprint, render_template
 
 error_bp = Blueprint('error', __name__)
 
-@error_bp.app_errorhandler(404)
-def not_found_error(error):
-    logging.error(error)
-    return render_template('404.html'), 404
 
-@error_bp.app_errorhandler(500)
-def internal_error(error):
-    logging.error(error)
-    return render_template('500.html'), 500
+@error_bp.app_errorhandler(Exception)
+def handle_error(error):
+    """Универсальная обработка ошибок."""
+    error_code = getattr(error, 'code', 500)
+    logging.error(f"Error: {error}, Code: {error_code}")
+    
+    return render_template('error.html', error_code=error_code, error_message=str(error)), error_code
