dc1e480f1dfdb6b3db8701ed109ceb54db2658c2
FlacSy <flacsy.x@gmail.com>
2024-12-07 18:36:25 +0100

    End develop srcimer logic

---
+++ frontend/static/js/error.js
@@ -0,0 +1,31 @@
+
+const scrimer = document.getElementsByClassName('scrimer')[0]
+const error_img = document.getElementsByClassName('error_img')[0]
+
+const gif = scrimer.getElementsByTagName('img')[0]
+const audio = scrimer.getElementsByTagName('audio')[0]
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function show_scrimer() {
+    scrimer.style.display = 'flex'
+    error_img.style.display = 'none'
+    audio.autoplay = true
+    audio.volume = 1.0
+    audio.play()
+}
+
+function main() {
+    audio.volume = 0.0
+    sleep(1000).then(() => {
+        show_scrimer()
+    }) 
+}
+
+
+document.addEventListener("click", function handleClick() {
+    document.removeEventListener("click", handleClick);
+    main();
+});
