da6c24aeba62e2b10aa5dd98e79edce8aec0152a
Jelly <lycomilopster@gmail.com>
2024-12-08 22:43:43 +0100

    Merge

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -23,41 +23,22 @@ document.getElementById("submit-button").addEventListener('click', async () => {
         if (info_response.status == 200) {
             const metadata = await info_response.json();
             await showInfo(metadata);
-        }
 
-    } catch (error) {
-        console.error('An error occurred:', error);
-    } finally {
-        button.disabled = false;
-        input.readOnly = false;
-    }
-});
-
-document.getElementById("download-button").addEventListener('click', async () => {
-    const input = document.getElementById("url-input");
-    const selectValue = document.getElementById("select-format").value;
-    const button = document.getElementById("download-button");
-
-    const inputValue = input.value;
-    if (!inputValue || !selectValue) return;
-
-    button.disabled = true;
-    input.readOnly = true;
-
-    try {
-        const download_response = await fetch('/download/file', {
-            method: 'GET',
+        const download_response = await fetch('/download', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                file_path: inputValue,
+                url: inputValue,
+                format: selectValue
             }),
         });
         if (download_response.status == 200) {
             await saveContent(download_response, selectValue);
         } else {
             alert("Error downloading content");
+        }
         }
 
     } catch (error) {
@@ -71,11 +52,9 @@ document.getElementById("download-button").addEventListener('click', async () =>
 async function saveContent(response, fileType) {
     try {
         const json = await response.json();
-        console.log(json);
         const fileUrl = json.file_url;
         const a = document.createElement("a");
         a.href = fileUrl;
-        a.download = fileType === 'mp3' ? 'audio.mp3' : 'video.mp4';
         a.click();
     } catch (error) {
         console.error('Error saving content:', error);
@@ -85,14 +64,11 @@ async function saveContent(response, fileType) {
 async function showInfo(metadata) {
     try {
         const infoElement = document.getElementById("info");
-        const imageElement = document.getElementById("video-thumbnail");
-        const textElement = document.getElementById("video-title");
-        const buttonElemtn = document.getElementById("download-button");
 
         if (metadata.thumbnail && metadata.title) {
-            imageElement.src = metadata.thumbnail;
-            textElement.innerText = metadata.title;
-            infoElement.style.display = 'flex';
+            infoElement.querySelector('img').src = metadata.thumbnail;
+            infoElement.querySelector('h1').innerText = metadata.title;
+            infoElement.style.display = 'block';
         } else {
             console.error('Invalid metadata:', metadata);
         }
