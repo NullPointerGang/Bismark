85eafa29b1cbf7e01ee095dc5c73ec702ecfb967
Jelly <lycomilopster@gmail.com>
2024-12-08 13:57:22 +0100

    Register and Login

---
+++ frontend/static/js/login.js
@@ -0,0 +1,36 @@
+document.getElementById("submit").addEventListener("click", async () => {
+    const usernameInput = document.getElementById("username")
+    const passwordInput = document.getElementById("password")
+
+    const username = usernameInput.value
+    const password = passwordInput.value
+
+    if (!username) return;
+    if (!password) return;
+
+    const button = document.getElementById("submit")
+
+    button.disabled = true;
+    usernameInput.readOnly = true;
+    passwordInput.readOnly = true;
+
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: usernameInput,
+            password: btoa(passwordInput)
+        }),
+    });
+
+    if (response.status == 200){
+        // Loged in
+        alert("Login successed")
+        window.location.href('/')
+    } else if (response.status == 401) {
+        // Not loged in
+        alert("Ti loh")
+    }
+})
---
+++ frontend/static/js/register.js
@@ -0,0 +1,39 @@
+document.getElementById("submit").addEventListener("click", async () => {
+    const usernameInput = document.getElementById("username")
+    const passwordInput = document.getElementById("password")
+
+    const username = usernameInput.value
+    const password = passwordInput.value
+
+    if (!username) return;
+    if (!password) return;
+
+    const button = document.getElementById("submit")
+
+    button.disabled = true;
+    usernameInput.readOnly = true;
+    passwordInput.readOnly = true;
+
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: usernameInput,
+            password: btoa(passwordInput)
+        }),
+    });
+
+    if (response.status == 200){
+        // Created
+        alert("Register successed")
+        window.location.href('/')
+    } else if (response.status == 409) {
+        // Conflict
+        alert("User already exists")
+    } else if (response.status == 400) {
+        // Other error
+        alert("Пашол нахуй чмо лишай")
+    }
+})
